056868f769f824a080689c586bc2c3e78f799ff2
TonNom <tegracesalumu@protonmail.com>
2025-10-08 17:37:40 +0200

    untracked box

--- exos/05_virtualbox_vm/main.tf
+++ exos/05_virtualbox_vm/main.tf
@@ -1,17 +1,24 @@
 terraform {
   required_providers {
     virtualbox = {
-      source  = "github.com/terra-farm/virtualbox"
-      version = "0.2.2"
+      source = "terra-farm/virtualbox"
+      version = "0.2.2-alpha.1"
     }
   }
 }
 
+
 provider "virtualbox" {}
 
-resource "virtualbox_vm" "ubuntu_vm" {
-  name   = "terraform_ubuntu"
-  image  = "https://app.vagrantup.com/ubuntu/boxes/jammy64/versions/20230413.0.0/providers/virtualbox.box"
-  cpus   = 1
-  memory = 1024
+resource "virtualbox_vm" "node" {
+  name      = "tp"
+  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  cpus      = 1
+  memory    = "512 mib"
+  
+  network_adapter {
+    type           = "hostonly"
+    host_interface = "vboxnet1"
+  }
+
 }
